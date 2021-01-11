f4be3705e53d257fa034a4fbd3495a68adc98b9f
krakra9 <56550740+Dany65@users.noreply.github.com>
2021-01-11 13:42:12 +0000

    Create QuoteAQuote.cpp

---
+++ QuoteAQuote.cpp
@@ -0,0 +1,15 @@
+//https://repl.it/@dany65/QuoteAQuote#main.cpp
+#include <iostream>
+int main() {
+  std:: string quote;
+  std:: string author;
+  
+
+  std::cout << "What is the quote? ";
+  getline(std::cin, quote);
+
+  std::cout << "Who said it? ";
+  getline(std::cin, author);
+
+  std::cout <<"\n" << author << " says: \"" << quote << "\" \n";
+}
